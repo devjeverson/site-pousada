e934e607f303fa7d05eb08c7f53bc806866633f9
devjeverson <developer.oliveirajv@gmail.com>
2025-07-05 21:44:21 -0300

    add components for about, header, main-content, road, rooter, and welcome with their respective templates and styles

---
+++ src/app/road/road.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-road',
+  imports: [],
+  templateUrl: './road.component.html',
+  styleUrl: './road.component.css'
+})
+export class RoadComponent {
+
+}
---
+++ src/app/rooter/rooter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooter',
+  imports: [],
+  templateUrl: './rooter.component.html',
+  styleUrl: './rooter.component.css'
+})
+export class RooterComponent {
+
+}
